11e06b9adb70b8821c62d237708f33c645cbcc94
Simone Cigagna <simonecigagna@gmail.com>
2021-05-19 16:16:07 +0200

    added automatic cutoff detection

---
+++ es2/json_parser.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+
+fname = "SSSP_efficiency.json"
+f = open(fname)
+parsed = json.load(f)
+
+
+# Return cutoff value for
+# the requested element.
+# SSSP_efficiency.json must be
+# present in the same folder as this
+# module.
+def cutoff(at_name):
+    return parsed[at_name]["cutoff"]
+
+# same as cutoff but for multiple
+# elements
+def cutoffs(at_names):
+    values = []
+    for name in at_names:
+        values.append(parsed[name]["cutoff"])
+    return values
