3bc245248ac043a591711787c3979c03bf2fabef
Simone Cigagna <simonecigagna@gmail.com>
2021-06-05 14:51:38 +0200

    fit output

---
+++ es3/different_grid.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import f90nml
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+
+def main():
+    # first run pw
+    # then run pw4gww
+    # repeat with different cell values
+
+
+if __name__ == "__main__":
+    main()
